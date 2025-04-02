0c382b2c1a28ce2ab895b04cb8db65ffa47cf52a
kaifpatel123 <kaifp0374@gmail.com>
2025-04-02 10:59:17 +0530

    Updated research page and fixed modal issue

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onclick = function(event) {
+    const modal = document.getElementById('imageModal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function closeModal() {
+    document.getElementById('imageModal').style.display = "none";
+}
